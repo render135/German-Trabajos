cf42a4fd6fa818f56d531960f7705c184111336c
SENA <SENA@ARMSCCCCTGOD136.FormacionSena.RED>
2025-03-19 11:16:54 -0500

    agregado funcion saludar

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function main(nombre) {
     console.log (`Bienvenido a mi programa ${nombre}`)
 }
+
+function saludaEstudiante() {
+    console.log(`Hola estudiante ${nombre}`)
+}
 main("Nicolas")
+saludaEstudiante("Nicolas")
